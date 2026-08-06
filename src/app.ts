445f34cfb696c9c162fdab28bf2a4c2db45243fd
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-08-06 11:57:47 +0200

    database connection

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,39 @@
-console.log("Hello via Bun!");
+import express from "express";
+import nunjucks from "nunjucks";
+import { connectDB, closeDB } from "./models/db";
+
+const app = express();
+const port = process.env.PORT;
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+app.set("view engine", "njk");
+
+app.get("/", (req, res) => {
+  res.render("_base.njk", { title: "Trail Guide" });
+});
+
+try {
+  await connectDB();
+  console.log("Database successfully connected");
+} catch (error) {
+  console.error("Failed to connect to database", error);
+  process.exit(1);
+}
+app.listen(port, () => {
+  console.log(`Server is runnung at http://localhost:${port}`);
+});
+
+process.on("SIGINT", async () => {
+  console.log("SIGINT received. Closing database connection...");
+  await closeDB();
+  process.exit(0);
+});
+
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM received. Closing database connection...");
+  await closeDB();
+  process.exit(0);
+});
---
+++ src/models/db.ts
@@ -0,0 +1,29 @@
+import { open, Database } from "sqlite";
+import sqlite3 from "sqlite3";
+
+const DB_FILE = process.env.DB_PATH as string;
+
+let db: Database | null = null;
+
+export async function connectDB(): Promise<Database> {
+  db = await open({
+    filename: DB_FILE,
+    driver: sqlite3.Database,
+  });
+
+  return db;
+}
+
+export function getDB(): Database {
+  if (!db) {
+    throw new Error("Database not connected. Call connectDB() first.");
+  }
+  return db;
+}
+
+export async function closeDB(): Promise<void> {
+  if (db) {
+    await db.close();
+    db = null;
+  }
+}
